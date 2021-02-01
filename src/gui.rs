8b95dd28eb18f95e526853116190b90dd4491870
Paul Blouët <paul.blouet@outlook.com>
2021-02-01 17:08:38 +0100

    wip merging thread/async

--- src/gui.rs
+++ src/gui.rs
@@ -1,23 +1,24 @@
 use anyhow::Result;
+use async_std::stream::StreamExt;
+use crossbeam::channel::{unbounded, Receiver, Sender};
 use druid::{
     widget::{Align, Button, Flex, Label, TextBox},
     AppDelegate, AppLauncher, Command, Data, DelegateCtx, Env, ExtEventSink, Handled, Lens,
-    Selector, Target, ValueType, Widget, WidgetExt, WindowDesc,
+    Selector, Target, Widget, WidgetExt, WindowDesc, WindowId,
 };
-use futures::{Stream, StreamExt};
-use std::{
-    fmt::Debug,
-    sync::mpsc::{channel, Receiver, Sender},
-    thread::spawn,
-};
+use futures::{stream, Stream};
+use log::info;
+use std::{fmt::Debug, thread::spawn};
 
-enum NetEvent {
+#[derive(Debug)]
+enum GuiEvent {
     Rename(String),
+    Exit,
 }
 
 // Gui Commands
 const RENAME: Selector<String> = Selector::new("rename");
-const RENAMED: Selector<String> = Selector::new("renamed");
+const RENAMED: Selector<()> = Selector::new("renamed");
 
 #[derive(Clone, Data, Lens)]
 struct HelloState {
@@ -38,8 +39,8 @@ struct Delegate {
     ev_snd: EvSend,
 }
 
-type EvSend = Sender<NetEvent>;
-type EvRecv = Receiver<NetEvent>;
+type EvSend = Sender<GuiEvent>;
+type EvRecv = Receiver<GuiEvent>;
 
 impl AppDelegate<HelloState> for Delegate {
     fn command(
@@ -50,22 +51,33 @@ impl AppDelegate<HelloState> for Delegate {
         data: &mut HelloState,
         _env: &Env,
     ) -> Handled {
+        dbg!(cmd);
         if let Some(name) = cmd.get(RENAME) {
             data.name = name.clone();
             Handled::Yes
-        } else if let Some(name) = cmd.get(RENAMED) {
+        } else if let Some(()) = cmd.get(RENAMED) {
             // need a clean way to handle errors (env? state?)
-            let _ = self.ev_snd.send(NetEvent::Rename(name.clone()));
+            self.ev_snd.send(GuiEvent::Rename(data.new_name.clone())).unwrap();
             Handled::Yes
         } else {
             Handled::No
         }
     }
+
+    fn window_removed(
+        &mut self,
+        _id: WindowId,
+        _data: &mut HelloState,
+        _env: &Env,
+        _ctx: &mut DelegateCtx,
+    ) {
+        self.ev_snd.send(GuiEvent::Exit).unwrap();
+    }
 }
 
 fn setup_gui() -> Result<(ExtEventSink, EvRecv)> {
-    let (hs, hr) = channel();
-    let (es, er) = channel::<NetEvent>();
+    let (hs, hr) = unbounded();
+    let (es, er) = unbounded::<GuiEvent>();
 
     spawn(move || -> Result<()> {
         let main_window = WindowDesc::new(build_root_widget)
@@ -90,7 +102,9 @@ fn build_root_widget() -> impl Widget<HelloState> {
         .with_placeholder("Who are we greeting?")
         .fix_width(200.)
         .lens(HelloState::new_name);
-    let submit = Button::new("Submit").on_click(|_, _, _| {});
+    let submit = Button::new("Submit").on_click(|ctx, _, _| {
+        ctx.submit_command(RENAMED)
+    });
 
     let layout = Flex::column()
         .with_child(label)
@@ -102,21 +116,36 @@ fn build_root_widget() -> impl Widget<HelloState> {
     Align::centered(layout)
 }
 
+#[derive(Debug)]
+enum Event<T: Debug> {
+    Net(T),
+    Gui(GuiEvent),
+}
+
 pub async fn run<T>(swarm: &mut T) -> Result<()>
 where
     T: Stream + Unpin,
     <T as Stream>::Item: Debug,
 {
     let (gui_handle, er) = setup_gui()?;
+    let gui_stream = stream::iter(er.try_iter());
+    // let mut stream = swarm
+    //     .map(|v| Event::Net(v))
+    //     .merge(gui_stream.map(|v| Event::Gui(v)));
+    let mut stream = gui_stream.map(|v| Event::<()>::Gui(v));
 
-    loop {
-        while let Ok(gui_event) = er.try_recv() {
-            match gui_event {
-                NetEvent::Rename(name) => gui_handle.submit_command(RENAME, name, Target::Auto)?,
+        while let Some(ev) = stream.next().await {
+            dbg!(&ev);
+            match ev {
+                Event::Net(net_event) => info!("{:?}", net_event),
+                Event::Gui(gui_event) => match gui_event {
+                    GuiEvent::Rename(name) => {
+                        gui_handle.submit_command(RENAME, name, Target::Auto)?
+                    },
+                    GuiEvent::Exit => break,
+                },
             }
         }
-        while let Some(net_event) = swarm.next().await {
-            println!("{:?}", net_event);
-        }
-    }
+
+        Ok(())
 }
